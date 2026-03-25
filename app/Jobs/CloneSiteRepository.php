9514c0c0b1f11f917a0a2bd7dd24e491a7c74440
agve95-hub <agve95@gmail.com>
2026-03-25 19:12:12 +0100

    Add clone repository job

---
+++ app/Jobs/CloneSiteRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Site;
+use App\Services\GitSyncService;
+use App\Services\ProjectDetector;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class CloneSiteRepository implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct(public int $siteId)
+    {
+    }
+
+    public function handle(GitSyncService $gitSyncService, ProjectDetector $projectDetector): void
+    {
+        $site = Site::findOrFail($this->siteId);
+        $site->update(['status' => 'cloning']);
+
+        $repoPath = $gitSyncService->clone($site);
+        $detected = $projectDetector->detect($repoPath);
+
+        $site->update([
+            'local_repo_path' => $repoPath,
+            'project_type' => $detected['project_type'],
+            'build_command' => $detected['build_command'],
+            'build_output_dir' => $detected['build_output_dir'],
+            'status' => 'ready',
+            'last_synced_at' => now(),
+            'meta' => [
+                'last_commit' => $gitSyncService->currentCommit($repoPath),
+            ],
+        ]);
+    }
+}
